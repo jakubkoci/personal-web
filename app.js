26159c2087f7bb84e9d61e7e46d09b5747a0ef2e
Jakub Koci <jakub.koci@gmail.com>
2022-10-30 12:30:23 +0100

    Update talks with AFJ workshop and pepe logic

--- app.js
+++ app.js
@@ -1,71 +1,88 @@
 const talks = [
   {
+    name: "Workshop: How To Build a Self-Sovereign Identity Agent With Hyperledger Aries Framework JavaScript",
+    date: "2022-09-14",
+    event: {
+      name: "Hyperledger Global Forum",
+      link: "https://hgf22.sched.com/event/15Bjb",
+    },
+  },
+  {
+    name: "pepe logic #53 | Decentralizovaná identita, hyperledger, blockchain a další",
+    date: "2021-12-18",
+    event: {
+      name: "pepe logic",
+      link: "https://www.youtube.com/c/pepelogic",
+    },
+    video: {
+      link: "https://www.youtube.com/watch?v=LbSDjJfj_do",
+      lang: "cz",
+    },
+  },
+  {
     name: "Building Self-sovereign Identity in South Africa",
     date: "2020-03-03",
     event: {
       name: "Hyperledger Global Forum",
-      link: "https://hgf20.sched.com/event/XodU/building-self-sovereign-identity-in-south-africa-jakub-koci-patrik-stas-absa"
+      link: "https://hgf20.sched.com/event/XodU/building-self-sovereign-identity-in-south-africa-jakub-koci-patrik-stas-absa",
     },
     video: {
       link: "https://www.youtube.com/watch?v=j18YW0N_-7w",
-      lang: "en"
-    }
+      lang: "en",
+    },
   },
   {
     name: "Decentralizovaná digitální identita",
     date: "2019-10-13",
     event: {
       name: "jOpenSpace",
-      link: "https://www.jopenspace.cz/"
+      link: "https://www.jopenspace.cz/",
     },
     video: {
       link: "https://www.youtube.com/watch?v=vpDNZvzQ8M4",
-      lang: "cz"
+      lang: "cz",
     },
     slides: {
-      link:
-        "https://s3-eu-west-1.amazonaws.com/files.jakubkoci.com/ssi-jopenspace.pdf",
-      lang: "cz"
-    }
+      link: "https://s3-eu-west-1.amazonaws.com/files.jakubkoci.com/ssi-jopenspace.pdf",
+      lang: "cz",
+    },
   },
   {
     name: "Blockchain in Banking: simplifying paperwork, data & security",
     date: "2019-03-20",
     event: {
       name: "Hyperledger Prague Meetup",
-      link: "https://www.meetup.com/Hyperledger-Prague/events/259558781/"
+      link: "https://www.meetup.com/Hyperledger-Prague/events/259558781/",
     },
     slides: {
-      link:
-        "https://www.slideshare.net/jakubkoci/digital-identity-on-blockchain",
-      lang: "en"
-    }
+      link: "https://www.slideshare.net/jakubkoci/digital-identity-on-blockchain",
+      lang: "en",
+    },
   },
   {
-    name:
-      "Blockchain: Self-sovereign identity, Sovrin Trust Framework and Hyperledger Indy",
+    name: "Blockchain: Self-sovereign identity, Sovrin Trust Framework and Hyperledger Indy",
     date: "2018-11-12",
     event: {
       name: "Hyperledger Copenhagen Meetup",
-      link: "https://www.meetup.com/Hyperledger-Copenhagen/events/255979103/"
-    }
+      link: "https://www.meetup.com/Hyperledger-Copenhagen/events/255979103/",
+    },
   },
   {
     name: "Software Quality",
     date: "2018-02-01",
     event: {
-      name: "BudgetBakers (internal talk)"
+      name: "BudgetBakers (internal talk)",
     },
     slides: {
       link: "https://www.slideshare.net/jakubkoci/software-quality-166895898",
-      lang: "en"
-    }
-  }
+      lang: "en",
+    },
+  },
 ];
 
 const talksSection = document.getElementById("talks-list");
 
-talks.forEach(talk => {
+talks.forEach((talk) => {
   const talkElement = document.createElement("li");
 
   const event = talk.event.link
@@ -93,7 +110,7 @@ function a(href, text) {
 function formatDate(date) {
   var options = {
     year: "numeric",
-    month: "long"
+    month: "long",
   };
   return new Intl.DateTimeFormat(navigator.language, options).format(date);
 }
